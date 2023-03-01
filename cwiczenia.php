f3d2c6af6a2b9c42bc1adf0a225c35889772f1a7
Jakub Mikolajczak <jakubmikolajczak1ti@gmail.com>
2023-03-01 17:10:28 +0100

    Plik z ćwiczeniami

---
+++ cwiczenia.php
@@ -0,0 +1,14 @@
+<?php
+function ex1()
+{
+    phpinfo();
+
+}
+function ex2()
+{
+    echo "Tomorrow I'll learn PHP global variables <br>";
+    echo "This is a bad command: del c:\\*.*";
+}
+ex2();
+// ex1();
+?>
