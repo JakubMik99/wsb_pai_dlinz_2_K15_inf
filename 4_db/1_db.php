946f9ee2d0977bfed3e450a98d83d8228d00bba0
mikolajczakj <jakubmikolajczak1ti@gmail.com>
2023-03-08 13:40:20 +0100

    Dodanie bazy danych

---
+++ 4_db/1_db.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Użytkownicy</title>
+</head>
+<body>
+    <h4>Użytkownicy z db</h4>
+    <?php
+    require_once "../scripts/connect.php";
+    $sql = "SELECT * FROM `users`;";
+    $result = $conn->query($sql);
+    // $user = $result->fetch_assoc();
+    // echo "Imię i nazwisko " . $user["firstName"] . " " . $user["lastName"]. "<br>";
+    while( $user = $result->fetch_assoc()){
+        echo <<< USER
+        Imię i nazwisko: $user[firstName] $user[lastName] ur: $user[bitthday]<br>
+        USER;
+    }
+    // print_r($user);
+    ?>
+</body>
+</html>
